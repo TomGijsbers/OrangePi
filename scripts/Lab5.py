8bc53b1e396261f676108610e6e23f1245ed4a44
TomGijsbers <r0878924@student.thomasmore.be>
2024-02-24 13:03:18 +0100

    lab5.2/5.4/5.5

--- scripts/Lab5.py
+++ scripts/Lab5.py
@@ -2,15 +2,15 @@ import time
 import wiringpi
 import sys
 
-def blink(_pin):
-    wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
-    time.sleep(0.5)
-    wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
-    time.sleep(0.5)
-    wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
-    time.sleep(0.5)
-    wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
-    time.sleep(0.5)
+# def blink(_pin):
+    # wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
+    # time.sleep(0.5)
+    # wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
+    # time.sleep(0.5)
+    # wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
+    # time.sleep(0.5)
+    # wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
+    # time.sleep(0.5)
 
 #SETUP
 print("Start")
@@ -24,35 +24,76 @@ wiringpi.pinMode(pin1, 1)            # Set pin to mode 1 ( OUTPUT )
 wiringpi.pinMode(pin2, 1)            # Set pin to mode 1 ( OUTPUT )
 wiringpi.pinMode(pin3, 1)            # Set pin to mode 1 ( OUTPUT )
 
-# wiringpi.digitalWrite(pin, 1)    # Write 1 ( HIGH ) to pin
+
+
+#  Practice 2
+
+pins = [pin,pin1,pin2,pin3]
+
+wiringpi.digitalWrite(pin, 1)
+wiringpi.digitalWrite(pin1, 1)
+wiringpi.digitalWrite(pin2, 1)
+wiringpi.digitalWrite(pin3, 1)
+time.sleep(0.1)
+wiringpi.digitalWrite(pin,0 )
+wiringpi.digitalWrite(pin1, 0)
+wiringpi.digitalWrite(pin2, 0)
+wiringpi.digitalWrite(pin3, 0)
+
+#  Practice 4
+# pins = [pin,pin1,pin2,pin3,pin3,pin2,pin1,pin]
+
+# for i in pins:
+#     wiringpi.digitalWrite(i,1)
+#     time.sleep(0.1)
+#     wiringpi.digitalWrite(i, 0) 
+
+
+# Practice 5
+# def control_leds(pins, state):
+#     for pin in pins:
+#         wiringpi.digitalWrite(pin, state)  # state: 1 for HIGH, 0 for LOW
+
+# # Define your pin groups
+# group1 = [pin, pin2]
+# group2 = [pin1, pin3]
+
+# # Control LEDs
+# control_leds(group1, 1)  # Turn on group 1
 # time.sleep(0.5)
-# wiringpi.digitalWrite(pin, 0)    # Write 1 ( HIGH ) to pin
+# control_leds(group1, 0)  # Turn off group 1
 # time.sleep(0.5)
-# wiringpi.digitalWrite(pin1, 1)    # Write 1 ( HIGH ) to pin
+
+# control_leds(group2, 1)  # Turn on group 2
 # time.sleep(0.5)
-# wiringpi.digitalWrite(pin1, 0)    # Write 1 ( HIGH ) to pin
+# control_leds(group2, 0)  # Turn off group 2
 # time.sleep(0.5)
+
+
+
+# wiringpi.digitalWrite(pin, 1)wiringpi.digitalWrite(pin, 1)    # Write 1 ( HIGH ) to pin
 # wiringpi.digitalWrite(pin2, 1)    # Write 1 ( HIGH ) to pin
 # time.sleep(0.5)
-# wiringpi.digitalWrite(pin2, 0)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin, 0)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin2, 0)  
 # time.sleep(0.5)
-# wiringpi.digitalWrite(pin3, 1)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin1, 1)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin3, 1) 
 # time.sleep(0.5)
-# wiringpi.digitalWrite(pin3, 0)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin1, 0)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin3, 0) 
 # time.sleep(0.5)
-
-
-wiringpi.digitalWrite(pin, 1)    # Write 1 ( HIGH ) to pin
-wiringpi.digitalWrite(pin2, 1)    # Write 1 ( HIGH ) to pin
-time.sleep(0.5)
-wiringpi.digitalWrite(pin, 0)    # Write 1 ( HIGH ) to pin
-wiringpi.digitalWrite(pin2, 0)  
-time.sleep(0.5)
-wiringpi.digitalWrite(pin1, 1)    # Write 1 ( HIGH ) to pin
-wiringpi.digitalWrite(pin3, 1) 
-time.sleep(0.5)
-wiringpi.digitalWrite(pin1, 0)    # Write 1 ( HIGH ) to pin
-wiringpi.digitalWrite(pin3, 0) 
+# wiringpi.digitalWrite(pin, 1)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin2, 1)    # Write 1 ( HIGH ) to pin
+# time.sleep(0.5)
+# wiringpi.digitalWrite(pin, 0)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin2, 0)  
+# time.sleep(0.5)
+# wiringpi.digitalWrite(pin1, 1)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin3, 1) 
+# time.sleep(0.5)
+# wiringpi.digitalWrite(pin1, 0)    # Write 1 ( HIGH ) to pin
+# wiringpi.digitalWrite(pin3, 0) 
 #cleanup
 print("Done")
 
