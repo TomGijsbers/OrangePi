68c4385fe4f24feecc36f14da642f4620a19bf81
TomGijsbers <r0878924@student.thomasmore.be>
2024-02-29 13:41:24 +0100

    LDR Simple

---
+++ scripts/LDR_simple.py
@@ -0,0 +1,21 @@
+import time
+import wiringpi
+
+# SETUP
+print("Start")
+
+
+# R=1K
+pinSwitch = 1
+wiringpi.wiringPiSetup() 
+wiringpi.pinMode(pinSwitch, 0)   
+
+# Infinite loop
+while True:
+    if(wiringpi.digitalRead(pinSwitch) == 1): #input is active low
+        print("Light")
+        time.sleep(0.5) #anti bouncing
+    else:
+        print("Dark")
+        time.sleep(0.5) #anti bouncing
+     
